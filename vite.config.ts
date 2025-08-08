1b824a0d9de543f362f2c8ad63cf93142231d1fd
Tim Hahn <hi@tmhhn.com>
2025-08-07 19:22:25 -0600

    Fixup path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: '/numbers-game/',
 	plugins: [react(), tailwindcss()],
 	test: {
 		globals: true,
