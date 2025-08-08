45b157c6b858106ce0ca277b11874656c65367f1
Tim Hahn <hi@tmhhn.com>
2025-08-07 19:23:52 -0600

    ohhh, path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: '/countdown-numbers-game-app/',
 	plugins: [react(), tailwindcss()],
 	test: {
 		globals: true,
